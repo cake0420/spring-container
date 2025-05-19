bacad64f1520fd4307f5dc6d693954ae0b8b6ff9
soowan <nick6767676767@gmail.com>
2025-05-19 12:41:38 +0900

    refactor: 프로그래밍 방식 싱글톤, 프로토타입 설정

--- src/test/java/com/cake7/spring/manualregistration/ManualRegistrationTest.java
+++ src/test/java/com/cake7/spring/manualregistration/ManualRegistrationTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.support.GenericApplicationContext;
 
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 @SpringBootTest(classes = {MyService.class, MyRepository.class})
 public class ManualRegistrationTest {
 
@@ -15,7 +18,28 @@ public class ManualRegistrationTest {
         context.registerBean(MyRepository.class);
         context.registerBean(MyService.class, () -> new MyService(context.getBean(MyRepository.class)));
         context.refresh();
-        MyService service = context.getBean(MyService.class);
-        service.printData();
+        MyService service1 = context.getBean(MyService.class);
+        MyService service2 = context.getBean(MyService.class);
+        System.out.println(service1);
+        System.out.println(service2);
+        assertSame(service1, service2);
+        service1.printData();
+    }
+
+    @Test
+    public void manualRegistrationTestWithProtoType() {
+        GenericApplicationContext context = new GenericApplicationContext();
+        context.registerBean(MyRepository.class);
+        context.registerBean(MyService.class, () ->
+                new MyService(context.getBean(MyRepository.class)),
+                beanDefinition -> beanDefinition.setScope("prototype"));
+        context.refresh();
+        MyService service1 = context.getBean(MyService.class);
+        MyService service2 = context.getBean(MyService.class);
+
+        System.out.println(service1);
+        System.out.println(service2);
+
+        assertNotSame(service1, service2);
     }
 }
